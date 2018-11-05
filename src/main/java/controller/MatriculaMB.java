976464e2c92064e20419641a30d2da00c732dcb3
EuPessoa <trabalhinhodoclavison@gmail.com>
2018-11-05 20:29:38 -0200

    [Matricula]
    Criada tela de matrícula

--- src/main/java/controller/MatriculaMB.java
+++ src/main/java/controller/MatriculaMB.java
@@ -10,8 +10,12 @@ import javax.inject.Inject;
 import javax.inject.Named;
 
 import org.omnifaces.cdi.Param;
+import org.omnifaces.util.Messages;
 
 import dto.InstituicaoDTO;
+import exception.EstudanteBusinessException;
+import exception.LoginException;
+import exception.MatriculaBusinessException;
 import model.Edital;
 import model.Instituicao;
 import model.Matricula;
@@ -24,56 +28,67 @@ import java.util.List;
 
 @ViewScoped
 @Named(value = "matriculaMB")
-public class MatriculaMB  implements Serializable {
-    private static final long serialVersionUID = -7785394172005232068L;
+public class MatriculaMB implements Serializable {
+	private static final long serialVersionUID = -7785394172005232068L;
 
-    @Inject
-    private MatriculaService matriculaService;
-    
-    private Matricula matricula;
-    
-    @Inject
-    private EditalService editalService;
-    
-    private Edital edital;
-    
-    @Inject
-    private InstituicaoService instituicaoService;
-    
-    private List<Instituicao> instituicoes = new ArrayList();
-    
-    @Inject @Param
-    private Long idEdital;
-    
-    @PostConstruct
-    public void init(){
-        novaMatricula();
-        carregarEdital();
-        listarInstituicao();
-    }
-    
-    public void novaMatricula() {
-    	matricula = new Matricula();
-    }
+	@Inject
+	private MatriculaService matriculaService;
 
-    private void carregarEdital() {
-    	if(idEdital!=null) {
-    		edital = editalService.listarEdital(idEdital);
-    	}
-    }
-    
-    public String obterPeriodo() {
-    	if(edital==null) {
-    		return "";
-    	}
-    	String aberto = edital.getFinalizado()==null?" (Aberto)":" (Fechado)"; 
-    	DateTimeFormatter formato  = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-    	return "Período de inscrição: "+formato.format(edital.getInicio())+" à "+formato.format(edital.getTermino())+aberto;
-    }
-    
-    public void listarInstituicao() {
-    	instituicoes = instituicaoService.listarInstituicao();
-    }
+	private Matricula matricula;
+
+	@Inject
+	private EditalService editalService;
+
+	private Edital edital;
+
+	@Inject
+	private InstituicaoService instituicaoService;
+
+	private List<Instituicao> instituicoes = new ArrayList();
+
+	@Inject
+	@Param
+	private Long idEdital;
+
+	@PostConstruct
+	public void init() {
+		novaMatricula();
+		carregarEdital();
+		listarInstituicao();
+	}
+
+	public void novaMatricula() {
+		matricula = new Matricula();
+	}
+
+	public void salvarMatricula(){
+		try {
+			matriculaService.salvar(matricula);
+			Messages.addInfo(null, "Matricula salva com sucesso");
+		} catch (MatriculaBusinessException e) {
+			e.getMessages().forEach(mensagem -> Messages.addError(null, mensagem));
+		}
+	}
+
+	private void carregarEdital() {
+		if (idEdital != null) {
+			edital = editalService.listarEdital(idEdital);
+		}
+	}
+
+	public String obterPeriodo() {
+		if (edital == null) {
+			return "";
+		}
+		String aberto = edital.getFinalizado() == null ? " (Aberto)" : " (Fechado)";
+		DateTimeFormatter formato = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		return "Período de inscrição: " + formato.format(edital.getInicio()) + " à "
+				+ formato.format(edital.getTermino()) + aberto;
+	}
+
+	public void listarInstituicao() {
+		instituicoes = instituicaoService.listarInstituicao();
+	}
 
 	public Matricula getMatricula() {
 		return matricula;
@@ -98,5 +113,5 @@ public class MatriculaMB  implements Serializable {
 	public void setInstituicoes(List<Instituicao> instituicoes) {
 		this.instituicoes = instituicoes;
 	}
-    
+
 }
--- src/main/java/service/MatriculaService.java
+++ src/main/java/service/MatriculaService.java
@@ -1,9 +1,10 @@
 package service;
 
+import exception.MatriculaBusinessException;
 import model.Matricula;
 
 public interface MatriculaService {
 	
-	void salvar(Matricula matricula);
+	void salvar(Matricula matricula) throws MatriculaBusinessException;
 	
 }
