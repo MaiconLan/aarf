155f81e9bfdb0f6fa24e22a26eeb02e4ba72c16b
Maicon Lanzendorf <maicon.lanzendorf@live.com>
2019-06-30 18:29:39 -0300

    Commit configs Heroku
    Teste com heroku

--- src/main/java/utils/Unit.java
+++ src/main/java/utils/Unit.java
@@ -11,6 +11,12 @@ public class Unit {
 	public static final String NOME_BASE_DADOS_LOCAL = "aarf-develop";
 	public static final String USUARIO_BASE_DADOS_LOCAL = "postgres";
 	public static final String SENHA_BASE_DADOS_LOCAL = "postgres";
+
+	// Dados da base local
+	public static final String IP_BASE_DADOS_HEROKU = "ec2-107-20-185-16.compute-1.amazonaws.com";
+	public static final String NOME_BASE_DADOS_HEROKU = "dc2llneqomh4eu";
+	public static final String USUARIO_BASE_DADOS_HEROKU = "shjheeuabowupi";
+	public static final String SENHA_BASE_DADOS_HEROKU = "c52149d880d90ecbd2ad62e029c6905cbabc8a69ff478482088e72514a3a940e";
 	
 	// Dados da base DigitalOcean
 	public static final String IP_BASE_DADOS_DO = "127.0.0.1";
@@ -23,6 +29,7 @@ public class Unit {
 	public static final String NUMERO_VERSAO = "1.5";
 	public static final String SNAPSHOT = "-1";
 
+	public static final String HEROKU = "Heroku";
 	public static final String DIGITAL_OCEAN = "Digital Ocean";
 	public static final String LOCAL = "localhost";
 
